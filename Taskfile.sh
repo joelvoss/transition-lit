b9e1a35971e1f8257ae348696c5f2141c62252e3
Joel Voss <mail@joelvoss.com>
2020-05-02 21:23:25 +0200

    feat(Taskfile): migrate to jvdx's `Taskfile`

---
+++ Taskfile.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+PATH=./node_modules/.bin:$PATH
+
+# //////////////////////////////////////////////////////////////////////////////
+
+# Default task (executed when no <task> argument was given)
+function default {
+  validate
+  build
+}
+
+function build {
+  jvdx build rollup 'src/index.tsx' -f cjs,esm -c
+}
+
+function format {
+  jvdx format "${@:1}"
+}
+
+function lint {
+  jvdx lint "${@:1}"
+}
+
+function test {
+  jvdx test
+}
+
+function validate {
+  format "${@:1}"
+  lint "${@:1}"
+  test
+}
+
+function clean {
+  jvdx clean dist "${@:1}"
+  (cd examples/basic && yarn clean)
+  (cd examples/transition-group && yarn clean)
+  (cd examples/css-transition && yarn clean)
+}
+
+# //////////////////////////////////////////////////////////////////////////////
+
+function help {
+  printf "$(_now) Usage: $0 <task> <args>\n\n"
+}
+
+function _now {
+  time=$(date +'%H:%M:%S')
+  printf "\e[2m[$time]\e[0m"
+}
+
+function _checkmark {
+  printf "\e[1m\e[32m✓\e[0m"
+}
+
+TASK=${@:-default}
+
+printf "$(_now) \e[1m\e[96mTaskfile\e[0m $TASK\n\n"
+${@:-default}
+printf "\n"
